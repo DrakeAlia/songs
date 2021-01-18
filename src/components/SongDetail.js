ee2e50c8bd534b7385285eab8336a6a43f13ff13
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-01-17 18:09:43 -0800

    conditional rendering

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongDetail = props => {
-    console.log(props);
-    return <div>Song Detail</div>;
+const SongDetail = ({ song }) => {
+    if (!song) {
+        return <div>Select a song</div>;
+    }
+
+    return  (
+        <div>
+            <h3>Details for:</h3>
+            <p>
+                Tilte: {song.title}
+                <br />
+                Duration: {song.duration}
+            </p>
+        </div>
+    );
 };
 
 
